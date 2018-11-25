278e98e78ebdd6bbdc2371f10b29364187b5ca0e
Mayur Barge <bargemayur05@gmail.com>
2018-11-25 14:58:54 -0800

    updating jenkins functionality to trigger jobs

--- index.js
+++ index.js
@@ -2,15 +2,16 @@
 
 const functions = require('firebase-functions');
 const {WebhookClient} = require('dialogflow-fulfillment');
-var jenkins = require('jenkins')({ baseUrl: 'http://<username>:<password>@ec2-54-167-103-17.compute-1.amazonaws.com:8080', crumbIssuer: true });
+var jenkins = require('jenkins')({ baseUrl: 'http://admin:SJSU2018@ec2-54-87-132-101.compute-1.amazonaws.com:8080', crumbIssuer: true });
 
 //GitHub Constants
 var github = require('octonode');
-var github_token = "";
+var github_token = "d48b7b3338d61530444466678a4d57c6ed08452d";
 var github_owner = "thevarunjain";
 var github_repo = "Project-Team-21";
 var github_org = "SJSU272LabF18";
 var client = github.client(github_token);
+
 
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
     const agent = new WebhookClient({ request, response });
@@ -28,26 +29,88 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
             agent.add('Unable to retrieve information about jobs');
         });
     }
-    function githubUsers (agent){
-        return getJenkinsJobs().then((data) => {
+    
+    function jenkinsJobStatus (agent){
+        const jobName = agent.parameters.jobName;
+        return getJenkinsJobStatus(jobName).then((data) => {
             agent.add(data);
         }).catch((err) => {
-            agent.add('Unable to retrieve information about jobs');
+            agent.add('Unable to retrieve information about job');
         });
     }
-    function githubIssue(agent){
-        return getGithubIssue().then((data) => {
-              agent.add(data);
-          }).catch((err) => {
-              agent.add("Error in connecting to GitHub");
-          });        
+    
+    function jenkinsJobTrigger (agent){
+        var jobName = "";
+        if (agent.parameters){
+            jobName = agent.parameters.jobName;
+        }
+        console.log(jobName);
+        if (!jobName) {
+            var context = agent.context.get('jenkins-job');
+            if (!context.parameters.jobName){
+                return agent.add('Job Name is missing. Sorry.');
+            }
+        }
+        return triggerJenkinsJob(jobName).then((data) => {
+            agent.add(data);
+        }).catch((err) => {
+            agent.add(`Unable to trigger ${jobName}`);
+        });
     }
+    
+    function githubIssue(agent){
+      return getGithubIssue().then((data) => {
+            agent.add(data);
+        }).catch((err) => {
+            agent.add("Error in connecting to GitHub");
+        });        
+    }
+    
     let intentMap = new Map();
     intentMap.set('jenkins.status', jenkinsStatus);
-    intentMap.set('jenkins.jobs', jenkinsJobs);
+    intentMap.set('jenkins.job', jenkinsJobs);    
+    intentMap.set('jenkins.job.status', jenkinsJobStatus);
+    intentMap.set('jenkins.job.trigger', jenkinsJobTrigger);
     intentMap.set('github.issue', githubIssue);
+
     agent.handleRequest(intentMap);
 });
+
+function triggerJenkinsJob(jobName){
+    return new Promise((resolve, reject) => {
+        var res = "";
+        jenkins.job.get(jobName, function(err, jobInfo) {
+            if (err) throw err;
+            var buildNumber = jobInfo.nextBuildNumber;
+            jenkins.job.build(jobName, function(err, queueItem) {
+                if (err) {
+                    reject(err);
+                }
+                res = `New build for ${jobName} triggered with build number ${buildNumber}`;
+                resolve(res);
+            });
+        });
+    });
+}
+
+function getJenkinsJobStatus(jobName){
+    return new Promise((resolve, reject) => {
+        var res = "";
+        jenkins.job.get(jobName, function(err, jobStatus) {
+            if (err) {
+                reject(err);
+            }
+            if (jobStatus.color != 'blue'){
+                res = `Jenkins job ${jobName} is in FAILED state`;
+            }
+            else {
+                res = `Jenkins job ${jobName} is in SUCCESSFUL state`;
+            }
+            resolve(res);
+        });
+    });
+}
+
 
 function getJenkinsJobs(){
     return new Promise((resolve, reject) => {
@@ -57,17 +120,17 @@ function getJenkinsJobs(){
                 reject(err);
             }
             var noOfJobs = jobs.length;
-            var failedJobs = 0;
-            for (var i = 0; i < noOfJobs; i++){
-                if (jobs[i].color != 'blue'){
-                    failedJobs += 1;
-                }
+            if (noOfJobs === 0){
+                res = "There are no jobs configured with Jenkins.";
             }
-            if (noOfJobs > 0){
-                res = "There are no jobs connfigured with Jenkins.";
+            else if (noOfJobs === 1){
+                res = `Job ${jobs[0].name} is configured with Jenkins.`;
             }
             else {
-                res = `There are ${noOfJobs} jobs connfigured with Jenkins.`;
+                res = `There are following ${noOfJobs} jobs configured with Jenkins.\n`;
+                for (var i = 0; i < noOfJobs; i++ ){
+                    res += `${i+1}. ${jobs[i].name}\n`;
+                }
             }
             resolve(res);
         });
@@ -75,34 +138,6 @@ function getJenkinsJobs(){
 }
 
 function getJenkinsStatus(){
-    return new Promise((resolve, reject) => {
-        var res = "";
-        jenkins.info(function(err, info) {
-            if (err) {
-                reject(err);
-            }
-            var mode = info.mode;
-            res += `Jenkins server is available and operating in ${mode} mode.`;
-            jenkins.node.list(function(err, nodeList) {
-                if (err) {
-                    
-                    reject(err);
-                }
-                var noOfAgents = nodeList.length - 1;
-                if (noOfAgents === 0){
-                    res += `You may want to configure slaves to make best of it.`;
-                } else if (noOfAgents === 1){
-                    res += `It's configured with ${noOfAgents} slave.`;
-                } else if (noOfAgents > 1){
-                    res += `It's configured with ${noOfAgents} slaves.`;
-                }
-                resolve(res);
-            });
-        });
-    });
-}
-
-function getGithubuser(){
     return new Promise((resolve, reject) => {
         var res = "";
         jenkins.info(function(err, info) {
